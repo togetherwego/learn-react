0ca4d7270514baa8dab9f3dceaa545fceff08374
firyalhuwaida <iyaalfiryal@gmail.com>
2022-04-17 13:45:15 +0700

    add: app & jsx

--- src/App.js
+++ src/App.js
@@ -2,24 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleNameCHange = () => {
+    const name = ['Firyal', 'Mikasa', 'Eren']
+    const int = Math.floor(Math.random() * 3)
+    return name[int]
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello {handleNameCHange()}!</p>
       </header>
     </div>
   );
-}
+} 
 
 export default App;
