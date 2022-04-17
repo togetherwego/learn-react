aea20159e9f4f6b4aba5699fd2e656191ae6db2c
firyalhuwaida <iyaalfiryal@gmail.com>
2022-04-17 14:15:16 +0700

    add: applying style

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React from 'react'
 
 const Header = () => {
+  const headerStyle = {
+    backgroundColor: 'blue',
+    color: '#fff',
+  }
   return (
-    <header>
+    <header style={headerStyle}>
       <h1>Groceries list</h1>
     </header>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
